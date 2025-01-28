da634b252a971b717282c9676240e56efa72aab0
Amit Kapila <amitkapila@hotmail.com>
2025-01-28 17:26:14 +0000

    7.3 Main test

--- java-web-dev-exercises/src/org/launchcode/java/demos/lsn6inheritance/Main.java
+++ java-web-dev-exercises/src/org/launchcode/java/demos/lsn6inheritance/Main.java
@@ -1,4 +1,13 @@
 package org.launchcode.java.demos.lsn6inheritance;
 
 public class Main {
+//    HouseCat garfield = new HouseCat("Garfield", 12);
+//    garfield.eat();
+//    System.out.println(garfield.isTired());   // prints true
+    public static void main(String[] args) {
+        HouseCat garfield = new HouseCat("Garfield",12);
+        garfield.eat();
+        System.out.println(garfield.isTired());
+    }
+
 }

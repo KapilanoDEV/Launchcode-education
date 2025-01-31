1575759b86b109a2d7962a123bbf1624fe2c224d
Amit Kapila <amitkapila@hotmail.com>
2025-01-31 02:01:08 +0000

    11 Exceptions

--- java-web-dev-exceptions/src/org/launchcode/exercises/Main.java
+++ java-web-dev-exceptions/src/org/launchcode/exercises/Main.java
@@ -1,5 +1,7 @@
 package org.launchcode.exercises;
 
+import org.launchcode.Temperature.TemperatureException;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Test out your Divide() function!
+        Divide(5,0);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -16,16 +19,37 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        for (Map.Entry<String, String> student : studentFiles.entrySet()){
+            CheckFileExtension(student.getValue());
+        }
     }
 
     public static void Divide(int x, int y)
     {
         // Write code here!
+       try {
+                System.out.println(x/y);
+            } catch (ArithmeticException e) {
+           System.out.println("You cannot divide by 0");
+            }
     }
+
 
     public static int CheckFileExtension(String fileName)
     {
         // Write code here!
+        if(fileName == null){
+            try {
+                throw new Exception("Filename is null");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (fileName.endsWith("java")) {
+            return 1;
+        } else {
+            return 0;
+        }
+
     }
 
 }
